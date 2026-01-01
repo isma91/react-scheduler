570e5d01a78ff1979f508acf94ddb27c046cfb59
isma91 <ismail@cezami.com>
2026-01-02 00:55:42 +0100

    add more exemple in the page 1 to display new props

--- src/Page1.tsx
+++ src/Page1.tsx
@@ -1,25 +1,50 @@
 import { Scheduler } from "./lib";
 import { EVENTS } from "./events";
-import { useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 import { SchedulerRef } from "./lib/types";
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const events = EVENTS.slice(3, 6);
 
 function Page1() {
   const calendarRef = useRef<SchedulerRef>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleRefresh = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  const customHeader = useCallback(
+    () => <Button onClick={handleRefresh}>{loading ? "LOADING..." : "Refresh"}</Button>,
+    [loading, handleRefresh]
+  );
 
   return (
     <>
-      <div>
-        <Link to="/">Go to home</Link>
-      </div>
+      <div style={{ height: "200vh" }}>
+        <div>
+          <Link to="/">Go to home</Link>
+          <button onClick={() => setLoading((prev) => !prev)}>
+            Toggle Loading ({loading ? "ON" : "OFF"})
+          </button>
+        </div>
 
-      <Scheduler
-        ref={calendarRef}
-        events={events}
-        // events={generateRandomEvents(200)}
-      />
+        <div style={{ marginTop: "50px" }}>
+          <Scheduler
+            ref={calendarRef}
+            events={events}
+            loading={loading}
+            customHeaderContent={customHeader}
+            stickyNavigation={true}
+            stickyNavigationOffset={64}
+            // events={generateRandomEvents(200)}
+          />
+        </div>
+      </div>
     </>
   );
 }
